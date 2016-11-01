11a014866cb135e472fbf9873db19a1e9dad2325
Zhao Yi <zhaoyi0113@gmail.com>
2016-11-01 12:55:06 +1100

    close file inputstream after reading them

--- src/main/java/com/southbanksoftware/Main.java
+++ src/main/java/com/southbanksoftware/Main.java
@@ -8,17 +8,34 @@ import java.io.IOException;
  */
 public class Main {
 
+
+    private static void closeFileStream(FileInputStream inputStream){
+        if(inputStream != null){
+            try {
+                inputStream.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
     public static void main(String[] args) {
         if (args.length < 2) {
             System.err.println("Please pass two data files");
             System.exit(1);
         }
         SqlQuery query = new SqlQuery();
+        FileInputStream t1Stream = null;
+        FileInputStream t2Stream = null;
         try {
-            String json = query.performSqlQuery(new FileInputStream(args[0]), new FileInputStream(args[1]));
+            t1Stream = new FileInputStream(args[0]);
+            t2Stream = new FileInputStream(args[1]);
+            String json = query.performSqlQuery(t1Stream, t2Stream);
             query.writeToFile(json);
         } catch (IOException e) {
             e.printStackTrace();
+        } finally {
+            closeFileStream(t1Stream);
+            closeFileStream(t2Stream);
         }
     }
 }
